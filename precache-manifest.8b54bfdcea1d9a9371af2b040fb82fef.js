d234a098b9b4925022dceb4b149ef9517488b72b
utkarshsingh99 <utkarshsingh369@gmail.com>
2021-02-04 19:24:41 +0530

    Updates

---
+++ precache-manifest.8b54bfdcea1d9a9371af2b040fb82fef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e73f267ba3a503da5e8b",
+    "url": "/Googlyzer/static/css/main.90f35c1d.chunk.css"
+  },
+  {
+    "revision": "e73f267ba3a503da5e8b",
+    "url": "/Googlyzer/static/js/main.e73f267b.chunk.js"
+  },
+  {
+    "revision": "4081c802336b55ddcc3e",
+    "url": "/Googlyzer/static/js/1.4081c802.chunk.js"
+  },
+  {
+    "revision": "7ba42b9801a8d2116ef1",
+    "url": "/Googlyzer/static/js/runtime~main.7ba42b98.js"
+  },
+  {
+    "revision": "74d7f4a3e6f5caee20290cf3e679eb15",
+    "url": "/Googlyzer/index.html"
+  }
+];
