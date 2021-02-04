fe24644bddb70688477ce067bf41cf5e33a4095e
utkarshsingh99 <utkarshsingh369@gmail.com>
2021-02-04 19:22:19 +0530

    Added Category charts

--- src/Components/Categories/index.js
+++ src/Components/Categories/index.js
@@ -1,17 +1,19 @@
 import React, { Component} from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { Radar } from 'react-chartjs-2';
+import { ButtonGroup, Button } from '@material-ui/core';
 
 import { buildChart2, buildChart3 } from '../../Actions/middleware';
 import {fetchDataLoading } from '../../Actions';
-import { XYPlot, ArcSeries, VerticalBarSeries, YAxis, XAxis } from 'react-vis';
-
-const PI = 3.14;
 
 class CategoryChart extends Component {
     constructor (props) {
         super(props);
         this.renderPieChart = this.renderPieChart.bind(this);
+        this.state = {
+            factor: 'COUNT'
+        }
     }
 
     componentDidMount() {
@@ -21,50 +23,54 @@ class CategoryChart extends Component {
     }
 
     renderPieChart () {
-        const { chartData: { data: chartData} } = this.props;
-        let arrayOfCategories = [];
-        let cout = 0;
-        for(let item in chartData) {
-            arrayOfCategories.push({ x: item, y: Number(chartData[item]) })
+        const { chartData: { data: rawData} } = this.props;
+        const factor = this.state.factor.toLowerCase()
+        const arrayOfCategories = [];
+        for(let item in rawData) {
+            arrayOfCategories.push({ name: item, number: Number(rawData[item][factor]) })
         }
         arrayOfCategories.sort((a,b) => b.y - a.y);
         const topCategories = arrayOfCategories.slice(0, 10);
-        // const totalTopVideos = topCategories.reduce((value, item) => value+item.number, 0);
-        // let startAngle = 0
-        // const pieChartData = topCategories.map(category => {
-        //     const angle = (2*PI*category.number)/totalTopVideos;
-        //     startAngle += angle; 
-        //     const innerRadius = parseInt(Math.random()*100);
-        //     const outerRadius = innerRadius + parseInt(Math.random()*100);
-        //     return {
-        //         angle0: startAngle,
-        //         angle,
-        //         radius0: 2,
-        //         radius: 5,
-        //         color: Math.random
-        //     }
-        // })
-        // console.log(pieChartData)
-        return topCategories;
+        const [labels, data] = [[], []]
+        for(let category of topCategories) {
+            labels.push(category.name);
+            data.push(category.number);
+        }
+
+        const chartData = {
+            labels,
+            datasets: [{
+                label: 'Category',
+                data,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+            }]
+        }
+        return chartData;
     }
 
     render() {
+        const options = {
+            scale: {
+                ticks: { beginAtZero: true },
+            },
+        }
         const categoryChartData = this.renderPieChart();
-        console.log(categoryChartData)
         return (
             <>
-                <XYPlot
-                    yType="ordinal"
-                    width={800}
-                    height={400}
-                >
-                    <YAxis />
-                    <XAxis />
-                    <VerticalBarSeries 
-                        className="vertical-bar-series-example"
-                        data={categoryChartData}
-                    />
-                </XYPlot>
+                <ButtonGroup 
+                    style={{ display: 'flex', justifyContent: 'center'}} 
+                    size="large"
+                    aria-label="large outlined primary button group">
+                    <Button onClick={() => this.setState({factor: 'COUNT'})}>
+                        By Number
+                    </Button>
+                    <Button onClick={() => this.setState({factor: 'DURATION'})}>
+                        By Duration
+                    </Button>
+                </ButtonGroup>
+               <Radar data={categoryChartData} options={options}/> 
             </>
         )
     }
@@ -74,8 +80,7 @@ const mapStateToProps = (state) => {
     console.log('MSTP : ',state)
     return {
         itemLoading: state.itemLoading,
-        chartData: state.chartReducer.chart2,
-        // chartLoaded: state.chartDataLoaded.chart1
+        chartData: state.chartReducer.chart2
     }
 }
 
